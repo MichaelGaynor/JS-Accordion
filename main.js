bbd15bfffa98ff207de3887a87b9ded1bb11063c
Michael Gaynor <jmichaelgaynor@gmail.com>
2015-10-19 13:38:30 -0400

    Finished remaking with add and remove class

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
-$('.content').hide();
+// $('.content').hide();
 
+// $(function(){
+//   $('.accordion-toggle').on('click', function(){
+//     $('.content').hide('slow');
+//     $(this).next('.content').show('slow');
+//   });
+// });
+
+// $(function(){
+
+// })
 $(function(){
-  $('.accordion-toggle').on('click', function(){
-    $('.content').hide('slow');
-    $(this).next('.content').show('slow');
+  $('.accordion-toggle.b').on('click', function(){
+    $('.content').removeClass('on').addClass('off');
+    $('.content.b').removeClass('off').addClass('on');
   });
 });
 
 $(function(){
+  $('.accordion-toggle.a').on('click', function(){
+    $('.content').removeClass('on').addClass('off');
+    $('.content.a').removeClass('off').addClass('on');
+  });
+});
 
-})
+$(function(){
+  $('.accordion-toggle.c').on('click', function(){
+    $('.content').removeClass('on').addClass('off');
+    $('.content.c').removeClass('off').addClass('on');
+  });
+});
